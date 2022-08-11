46260681bb37d835f23aebfb0544b024c3c91738
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-08-11 13:22:25 +0300

    fixed a bug

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const warning = document.querySelector('.warning');
 // });
 
 passwordConfirm.addEventListener('keyup', (e) => {
+        if(password.value.length >= 8 && passwordConfirm.value.length >= 0) {
+            if(passwordConfirm.value === password.value) {
+            warning.innerText = '';
+            password.style.border = '1px solid green';
+            passwordConfirm.style.border = '1px solid green';
+        } else {
+            warning.innerText = '* passwords do not match'
+        }
+    } else {
+        warning.innerText = '* passwords do not match'
+    } 
+})
+
+password.addEventListener('keyup', (e) => {
     if(password.value.length >= 8 && passwordConfirm.value.length >= 0) {
         if(passwordConfirm.value === password.value) {
         warning.innerText = '';
@@ -16,6 +30,8 @@ passwordConfirm.addEventListener('keyup', (e) => {
     } else {
         warning.innerText = '* passwords do not match'
     }
-    } 
+} else {
+    warning.innerText = '* passwords do not match'
+} 
 })
 
