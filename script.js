3a6b2e550f1125af711448f2a9dcbcf84aa1338a
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-08-11 13:04:40 +0300

    added password check

---
+++ script.js
@@ -0,0 +1,21 @@
+// Variables
+let password = document.querySelector('#password');
+let passwordConfirm = document.querySelector('#password-confirm');
+const warning = document.querySelector('.warning');
+
+// password.addEventListener('keyup', (e) => {
+//     warning.innerText = password.value;
+// });
+
+passwordConfirm.addEventListener('keyup', (e) => {
+    if(password.value.length >= 8 && passwordConfirm.value.length >= 0) {
+        if(passwordConfirm.value === password.value) {
+        warning.innerText = '';
+        password.style.border = '1px solid green';
+        passwordConfirm.style.border = '1px solid green';
+    } else {
+        warning.innerText = '* passwords do not match'
+    }
+    } 
+})
+
